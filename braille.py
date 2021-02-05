a16010d03916e72aeecdefbec7e61d865a233727
tun <tun@disroot.org>
2021-02-05 20:52:34 +0100

    Generalize and write brute-forcer

--- braille.py
+++ braille.py
@@ -10,164 +10,6 @@ import unicodedata as ud
 import sympy as sp
 
 
-# STRING = '0123456789ab'
-# STRING = range(2 * 6)
-# STRING = '0123456789abcdefgh'
-# STRING = range(3 * 6)
-# START = np.array(list(STRING))
-
-
-# row-wise intra-glyph
-
-# 01 67 cd
-# 23 89 ef
-# 45 ab gh
-
-# 1cr 1cc 1br 6bc 3gr 2gc
-
-def row_wise_intra_glyph(array):
-    return np.reshape(array, (-1, 3, 2))
-
-
-def d_row_wise_intra_glyph(array):
-    return np.ravel(array)
-
-
-# column-wise intra-glyph
-
-# 03 69 cf
-# 14 7a dg
-# 25 8b eh
-
-
-def col_wise_intra_glyph(array):
-    array = np.reshape(array, (-1, 2, 3))
-    return np.transpose(array, (0, 2, 1))
-
-
-def d_col_wise_intra_glyph(array):
-    array = np.transpose(array, (0, 2, 1))
-    return np.ravel(array)
-
-
-# row-wise inter-glyph
-
-# 01 23 45
-# 67 89 ab
-# cd ef gh
-
-def row_wise_inter_glyph(array):
-    array = np.reshape(array, (3, -1, 2))
-    return np.transpose(array, (1, 0, 2))
-
-
-def d_row_wise_inter_glyph(array):
-    array = np.transpose(array, (1, 0, 2))
-    return np.ravel(array)
-
-
-# col-wise inter-glyph
-
-# 09 3c 6f
-# 1a 4d 7g
-# 2b 5e 8h
-
-def col_wise_inter_glyph(array):
-    array = np.reshape(array, (2, -1, 3))
-    return np.transpose(array, (1, 2, 0))
-
-
-def d_col_wise_inter_glyph(array):
-    array = np.transpose(array, (2, 0, 1))
-    return np.ravel(array)
-
-
-def catz_order(array):
-    array = np.reshape(array, (1, 2, 3, -1, 2))
-    return np.transpose(array, (0, 1, 3, 2, 4))
-
-
-# (<row>x<column><name><order>){3}
-def parse_spec(spec):
-    position = {'c': 0, 'b': 1, 'g': 2}
-    sizes = [None] * 6
-    axes = [None] * 6
-
-    for i, part in enumerate(spec.split(' ')):
-        size, name, order = part[:-2], part[-2], part[-1]
-
-        j = position[name]
-        size = tuple(map(int, size.split('x')))
-        size = size if order == 'r' else reversed(size)
-        paxes = (2 * i, 2 * i + 1)
-        paxes = paxes if order == 'r' else reversed(paxes)
-
-        sizes[2 * i:2 * i + 2] = size
-        axes[2 * j:2 * j + 2] = paxes
-
-    return sizes, axes
-
-
-# (<size><name>){6}
-def parse_spec2(spec):
-    position = {'cr': 0, 'cc': 1, 'br': 2, 'bc': 3, 'gr': 4, 'gc': 5}
-    sizes = [None] * 6
-    axes = [None] * 6
-
-    for i, part in enumerate(spec.split(' ')):
-        size, name = part[:-2], part[-2:]
-
-        j = position[name]
-        size = int(size)
-        axis = i
-
-        sizes[i] = size
-        axes[j] = axis
-
-    return sizes, axes
-
-
-def unpack_braille(array, conf):
-    sizes, axes = conf
-    return np.transpose(np.reshape(array, sizes), axes)
-
-
-def rev_conf(conf):
-    sizes, axes = conf
-
-    sizes2 = [None] * len(sizes)
-    axes2 = [None] * len(axes)
-
-    for i, j in enumerate(axes):
-        sizes2[i] = sizes[j]
-        axes2[i] = axes[j]
-
-    return sizes2, axes2
-
-
-def pack_braille(array, conf):
-    _, axes = rev_conf(conf)
-    return np.ravel(np.transpose(array, axes))
-
-
-def test_unpack_pack(array, conf):
-    unpacked = unpack_braille(array, conf)
-    packed = pack_braille(unpacked, conf)
-
-    return unpacked, packed
-
-
-def test_num(conf, n=36):
-    return test_unpack_pack(np.fromiter(range(n), int), conf)
-
-
-import string
-
-def test_str(conf, s=None):
-    s = s or string.digits + string.ascii_lowercase
-    return test_unpack_pack(np.fromiter(s, 'U1'), conf)
-
-
 BRAILLE = {
     'a': '100000',
     'b': '101000',
@@ -219,63 +61,46 @@ BRAILLE_CHARS = make_braille_chars()
 BRAILLE_CHARS_REV = {v: k for k, v in BRAILLE_CHARS.items()}
 
 
-def encode_row_wise_intra_glyph(string):
-    string = it.chain.from_iterable(map(lambda l: BRAILLE[l], string))
-    array = np.reshape(np.fromiter(string, 'U1'), (-1, 3, 2))
-    return ''.join(d_row_wise_intra_glyph(array))
+######################################################################
+###
+### Permutations
 
 
-def decode_row_wise_intra_glyph(string):
-    # TODO: use fromiter
-    array = np.reshape(row_wise_intra_glyph(np.array(list(string))), (-1, 6))
-    return ''.join(map(lambda g: BRAILLE_REV[''.join(g)], array))
+def make_perm(src, dst):
+    return tuple(src.index(marker) for marker in dst)
 
 
-def encode_col_wise_intra_glyph(string):
-    string = it.chain.from_iterable(map(lambda l: BRAILLE[l], string))
-    array = np.reshape(np.fromiter(string, 'U1'), (-1, 3, 2))
-    return ''.join(d_col_wise_intra_glyph(array))
+def apply_perm(seq, perm):
+    return tuple(seq[i] for i in perm)
 
 
-def decode_col_wise_intra_glyph(string):
-    array = np.reshape(col_wise_intra_glyph(np.array(list(string))), (-1, 6))
-    return ''.join(map(lambda g: BRAILLE_REV[''.join(g)], array))
+def reverse_perm(perm):
+    final = [None] * len(perm)
+
+    for i, j in enumerate(perm):
+        final[j] = i
+
+    return tuple(final)
 
 
-def encode_row_wise_inter_glyph(string):
-    string = it.chain.from_iterable(map(lambda l: BRAILLE[l], string))
-    array = np.reshape(np.fromiter(string, 'U1'), (-1, 3, 2))
-    return ''.join(d_row_wise_inter_glyph(array))
+######################################################################
+###
+### Packing
 
 
-def decode_row_wise_inter_glyph(string):
-    array = np.reshape(row_wise_inter_glyph(np.array(list(string))), (-1, 6))
-    return ''.join(map(lambda g: BRAILLE_REV[''.join(g)], array))
+def unpack_braille(array, conf):
+    sizes, axes = conf
+    return np.transpose(np.reshape(array, sizes), reverse_perm(axes))
 
 
-def encode_col_wise_inter_glyph(string):
-    string = it.chain.from_iterable(map(lambda l: BRAILLE[l], string))
-    array = np.reshape(np.fromiter(string, 'U1'), (-1, 3, 2))
-    return ''.join(d_col_wise_inter_glyph(array))
+def pack_braille(array, conf):
+    _, axes = conf
+    return np.ravel(np.transpose(array, axes))
 
 
-def decode_col_wise_inter_glyph(string):
-    array = np.reshape(col_wise_inter_glyph(np.array(list(string))), (-1, 6))
-    return ''.join(map(lambda g: BRAILLE_REV[''.join(g)], array))
-
-
-def block_params(n):
-    factors = list(it.chain.from_iterable(
-        [p] * m for p, m in sp.factorint(n).items()))
-
-    partitions = it.takewhile(lambda p: len(p) <= 4, mit.partitions(factors))
-    partitions = set(tuple(sorted(map(math.prod, partition)))
-                     for partition in partitions)
-    partitions = (tuple(partition) + (1,) * (4 - len(partition))
-                  for partition in partitions)
-    partitions = set(it.chain.from_iterable(map(it.permutations, partitions)))
-
-    yield from partitions
+######################################################################
+###
+### Encoding and decoding
 
 
 # (packed) bitstring '0101010101010'
@@ -285,74 +110,451 @@ def block_params(n):
 # (packed) string
 
 
-def try_decode(array, conf):
+def decode(array, conf):
     array = unpack_braille(array, conf)
     decoded = np.zeros(array.shape[:-2], 'U1')
 
     try:
         for i in np.ndindex(decoded.shape):
             decoded[i] = BRAILLE_REV[''.join(np.ravel(array[i]))]
-    except KeyError:
+    except KeyError as e:
         return None
 
     return decoded
 
 
-def brute(string):
+def decode_str(string, conf):
+    return decode(np.fromiter(string, 'U1'), conf)
+
+
+######################################################################
+###
+### Brute-forcing
+
+
+def all_block_params(n):
+    factors = list(it.chain.from_iterable(
+        [p] * m for p, m in sp.factorint(n).items()))
+
+    partitions = it.takewhile(lambda p: len(p) <= 4, mit.partitions(factors))
+    partitions = set(tuple(sorted(map(math.prod, partition)))
+                     for partition in partitions)
+    partitions = (tuple(partition) + (1,) * (4 - len(partition))
+                  for partition in partitions)
+    # TODO: generate permutations that take into account repetition of elements
+    partitions = set(it.chain.from_iterable(map(it.permutations, partitions)))
+
+    yield from partitions
+
+
+def all_confs(string):
     n = len(string)
     assert n % 6 == 0
 
-    array = np.fromiter(string, 'U1')
-
-    for cr, cc, br, bc in block_params(n // 6):
+    for cr, cc, br, bc in all_block_params(n // 6):
         sizes = (cr, cc, br, bc, 3, 2)
         axes = tuple(range(6))
 
         for sizes, axes in zip(it.permutations(sizes), it.permutations(axes)):
-            res = try_decode(array, (sizes, axes))
-
-            if res is not None:
-                yield ''.join(np.ravel(res))
+            yield sizes, axes
 
 
-def something(string):
-    return set(brute(string))
+def brute(string):
+    array = np.fromiter(string, 'U1')
+
+    for conf in all_confs(string):
+        res = decode(array, conf)
+
+        if res is not None:
+            yield res, conf
 
 
-# print(decode_row_wise_intra_glyph(encode_row_wise_intra_glyph('hello')))
-# print(decode_col_wise_intra_glyph(encode_col_wise_intra_glyph('hello')))
-# print(decode_row_wise_inter_glyph(encode_row_wise_inter_glyph('hello')))
-# print(decode_col_wise_inter_glyph(encode_col_wise_inter_glyph('hello')))
+def brute1(string):
+    return set(''.join(np.ravel(decoded)) for decoded, _ in brute(string))
+
+
+######################################################################
+###
+### Utility
+
+
+# TODO: test whether there exists a > 3-tier structure that can't be captured
+# by a <= 3-tier structure
+
+# 1cr 1cc 3br 3bc 3gr 2gc
+# 1br 1bc 3cr 3cc 3gr 2gc
+
+
+def html(array, conf):
+    def rec(array, depth):
+        if type(array) is not np.ndarray:
+            return str(array)
+
+        row = []
+
+        for i in range(len(array)):
+            elem = rec(array[i], depth + 1)
+            row.append('<td class="depth{}">{}</td>'.format(depth, elem))
+
+        tr = '<tr class="depth{}">{}</tr>'.format(depth, '\n'.join(row))
+        return '<table class="depth{}">{}</table>'.format(depth, tr)
+
+    return rec(array, 0)
+
+
+def braille_html(array, conf):
+    assert array.ndim >= 3
+
+    def rec(array, depth):
+        if array.ndim == 2:
+            nr, nc = array.shape
+            table = []
+
+            for r in range(nr):
+                row = []
+
+                for c in range(nc):
+                    row.append('<td class="depth{}">{}</td>'.format(
+                        depth, array[r][c]))
+
+                table.append('<tr class="depth{}">{}</tr>'.format(
+                    depth, '\n'.join(row)))
+
+            return '<table class="depth{}">{}</table>'.format(
+                depth, '\n'.join(table))
+
+        row = []
+
+        for i in range(len(array)):
+            elem = rec(array[i], depth + 1)
+            row.append('<td class="depth{}">{}</td>'.format(depth, elem))
+
+        tr = '<tr class="depth{}">{}</tr>'.format(depth, '\n'.join(row))
+        return '<table class="depth{}">{}</table>'.format(depth, tr)
+
+    return rec(array, 0)
+
+
+# array = np.fromiter(range(54), int)
+# sizes, axes = (3, 3, 3, 2, 1, 1), (0, 2, 4, 5, 3, 1)
+# print(format_conf((sizes, axes)))
+# print(np.reshape(array, sizes))
+# print(np.transpose(np.reshape(array, sizes), axes))
+
+
+# array = np.fromiter(range(54), int)
+# 3cr 1cc 2gc 3br 1bc 3gr
+# sizes, axes = (3, 1, 2, 3, 1, 3), (0, 1, 5, 2, 3, 4)
+# print(unpack_braille(array, (sizes, axes)).shape)
+# print(html(unpack_braille(array, (sizes, axes)), (sizes, axes)))
+# shape = (3, 1, 1, 3, 2, 3)
+
+# print(np.reshape(array, sizes))
+# print(np.transpose(np.reshape(array, sizes), reverse_perm(axes)))
+
+# def test(string):
+#     n = len(string)
+#     assert n % 6 == 0
+
+#     for cr, cc, br, bc in all_block_params(n // 6):
+#         sizes = (cr, cc, br, bc, 3, 2)
+#         axes = tuple(range(6))
+
+#         for sizes, axes in zip(it.permutations(sizes), it.permutations(axes)):
+#             yield sizes, axes, apply_perm(sizes, axes)
+
+
+def doit(string):
+    packed = np.fromiter(string, 'U1')
+    array = np.fromiter(range(len(string)), int)
+
+    with open('braille.html', 'w') as f:
+        f.write('''<!doctype html>
+<html>
+<head>
+<style>
+    table {
+        border: 2px solid;
+        padding: 10px;
+        border-radius: 0px;
+        text-align: center;
+    }
+
+    table.depth0 {
+        border-color: #1cc970;
+    }
+
+    table.depth1 {
+        border-color: #e3db3d;
+    }
+
+    table.depth2 {
+        border-color: #1c87c9;
+    }
+</style>
+</head>
+<body>
+''')
+
+        f.write('''<h1>String</h1>
+{}</br>
+{}'''.format(packed, array))
+
+        for i, (_, conf) in enumerate(brute(string)):
+            array = unpack_braille(array, conf)
+            print(i, array.shape, conf[1])
+            sizes, axes = conf
+            f.write('<h1>Table {} ({}, {})</h1>'.format(i + 1, sizes, axes))
+            axes = tuple(a for a in axes if a not in (4, 5)) + (4, 5)
+            f.write(braille_html(array, conf))
+            f.write('\n')
+
+        f.write('''</body>
+</html>
+''')
+
+
+def format_decoded(decoded):
+    cr, cc, br, bc = decoded.shape
+    grid = [[None] * (cc * bc) for _ in range(cr * br)]
+
+    for i in np.ndindex(decoded.shape):
+        ci, cj, bi, bj = i
+        grid[ci * br + bi][cj * bc + bj] = decoded[i]
+
+    return '\n'.join(map('\t'.join, grid))
+
+
+# # (<row>x<column><name><order>){3}
+# def parse_spec(spec):
+#     position = {'c': 0, 'b': 1, 'g': 2}
+#     sizes = [None] * 6
+#     axes = [None] * 6
+
+#     for i, part in enumerate(spec.split(' ')):
+#         size, name, order = part[:-2], part[-2], part[-1]
+
+#         j = position[name]
+#         size = tuple(map(int, size.split('x')))
+#         size = size if order == 'r' else reversed(size)
+#         paxes = (2 * i, 2 * i + 1)
+#         paxes = paxes if order == 'r' else reversed(paxes)
+
+#         sizes[2 * i:2 * i + 2] = size
+#         axes[2 * j:2 * j + 2] = paxes
+
+#     return sizes, axes
+
+
+# (<size><name>){6}
+def parse_conf_spec(spec):
+    position = {'cr': 0, 'cc': 1, 'br': 2, 'bc': 3, 'gr': 4, 'gc': 5}
+    sizes = [None] * 6
+    axes = [None] * 6
+
+    for i, part in enumerate(spec.split(' ')):
+        size, name = part[:-2], part[-2:]
+
+        j = position[name]
+        size = int(size)
+        axis = i
+
+        sizes[i] = size
+        axes[j] = axis
+
+    return sizes, axes
+
+
+def format_conf(conf):
+    names = ['cr', 'cc', 'br', 'bc', 'gr', 'gc']
+    return ' '.join('{}{}'.format(size, names[axis])
+                    for size, axis in zip(*conf))
+
 
 PACKED = np.fromiter('101010110110000010101001100111101010100101011011001010', 'U1')
 
+# 101010110110000010101001100111101010100101011011001010
+#
+# '1cr 3cc 3gr 1br 3bc 2gc'
+#
+# 01 23 45 ij kl mn AB CD EF
+# 67 89 ab op qr st GH IJ KL
+# cd ef gh uv wx yz MN OP QR
+#
 # 10 10 10 10 10 01 10 01 01
 # 11 01 10 10 01 11 01 10 11
 # 00 00 10 10 10 10 00 10 10
 
-# 01 23 45 67 89 ab
-# cd ef gh ij kl mn
-# op qr st uv wx yz
-
+# reversed(gcol, bcol, brow, grow, ccol, crow)
+# crow ccol grow brow bcol gcol
+# '1cr 2cc 3gr 1br 3bc 2gc'
+#
 # 01 23 45 ij kl mn
 # 67 89 ab op qr st
 # cd ef gh uv wx yz
 
-# reversed(gcol, bcol, brow, grow, ccol, crow)
-# crow ccol grow brow bcol gcol
-# '1cr 2cc 3gr 1br 3bc 2gc'
+PACKED = np.fromiter('111000010010111101111000101011001000011011001011101010001000010111101000101101011010011010', 'U1')
+PACKEDN = np.fromiter(range(90), int)
 
-import string
+def encode(string):
+    return ''.join(map(lambda c: BRAILLE[c], string))
 
-STRING = string.digits + string.ascii_lowercase
+# congratulations
 
-# row, glyph, col
-# array = np.reshape(array, (3, -1, 2))
-# glyph, row, col
-# return np.transpose(array, (1, 0, 2))
+# ---
 
-RES = np.fromiter(STRING, 'U1')
-# brow, bcolumn, row, glyph, column
-RES = np.reshape(RES, (1, 2, 3, -1, 2))
-RES = np.transpose(RES, (0, 1, 3, 2, 4))
-print(RES)
+# 110000
+# 100110
+# 110110
+# 111100
+# 101110
+# 100000
+# 011110
+# 100011
+# 101010
+# 100000
+# 011110
+# 011000
+# 100110
+# 110110
+# 011010
+
+# ---
+
+# 110000 100110
+# 110110 111100
+# 101110 100000
+# 011110 100011
+# 101010 100000
+# 011110 011000
+# 100110 110110
+# 011010
+
+# ---
+
+# 11 10
+# 00 01
+# 00 10
+
+# 11 11
+# 01 11
+# 10 00
+
+# 10 10
+# 11 00
+# 10 00
+
+# 01 10
+# 11 00
+# 10 11
+
+# 10 10
+# 10 00
+# 10 00
+
+# 01 01
+# 11 10
+# 10 00
+
+# 10 11
+# 01 01
+# 10 10
+
+# 01 xx
+# 10 xx
+# 10 xx
+
+# ---
+
+# 11 10 00 01 00 10 11 11 01 11 10 00 10 10 11 00 10 00 01 10 11 00 10 11
+# 10 10 10 00 10 00 01 01 11 10 10 00 10 11 01 01 10 10 01 xx 10 xx 10 xx
+
+# ---
+
+# ###
+
+# 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&/()=?*_[]{}<>@+-.:,;žć
+
+# ---
+
+# 01 67 23 89 45 ab cd ij ef kl gh mn op uv qr wx st yz AB GH CD IJ EF
+# KL MN ST OP UV QR WX YZ %& !" /( #$ )= ?* }< _[ >@ ]{ +- .: ,; žć
+
+# ###
+
+# 01 67 23 89 45 ab cd ij ef kl gh mn op uv qr wx st yz AB GH CD IJ EF KL
+# MN ST OP UV QR WX YZ %& !" /( #$ )= [] >_ {} -+ @< *? xx xx xx
+
+# 01 67
+# 23 89
+# 45 ab
+
+# cd ij
+# ef kl
+# gh mn
+
+# op uv
+# qr wx
+# st yz
+
+# AB GH
+# CD IJ
+# EF KL
+
+# MN ST
+# OP UV
+# QR WX
+
+# YZ %&
+# !" /(
+# #$ )=
+
+# [] >_
+# {} -+
+# @< *?
+
+# xx
+# xx
+# xx
+
+# reversed('2gc 3gr 15bc 1br 1cr 1cc')
+# '1cc 1cr 1br 15bc 3gr 2gc'
+#
+# 01 67 cd ij op uv AB GH MN ST YZ %& [] >_ xx
+# 23 89 ef kl qr wx CD IJ OP UV !" /( {} -+ xx
+# 45 ab gh mn st yz EF KL QR WX #$ )= @< *? xx
+
+PACKED2 = np.fromiter('111000010010111101111000101011001000011011001011101010001000010111101000101101011010', 'U1')
+
+# '7br 3gr 1cr 2bc 1cc 2gc'
+
+# 01 23
+# 45 67
+# 89 ab
+
+# cd ef
+# gh ij
+# kl mn
+
+# op qr
+# st uv
+# wx yz
+
+# AB CD
+# EF GH
+# IJ KL
+
+# MN OP
+# QR ST
+# UV WX
+
+# YZ !"
+# #$ %&
+# /( )=
+
+# ?* _[
+# ]{ }<
+# >@ +-
+
+# for d, _ in brute('111000010010111101111000101011001000011011001011101010001000010111101000101101011010'):
+#     print(format_decoded(d))
